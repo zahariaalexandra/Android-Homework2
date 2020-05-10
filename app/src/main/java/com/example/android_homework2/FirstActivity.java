ed954b3bb7747abfc9315f7f14addf4eb0bd4fc3
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-05-10 23:21:32 +0300

    Add new fragment
    Change primary colors
    Add dependencies for the recyclerview widget
    Create RecyclerView fragment layout
    Add the new fragment to the activity

--- app/src/main/java/com/example/android_homework2/FirstActivity.java
+++ app/src/main/java/com/example/android_homework2/FirstActivity.java
@@ -1,6 +1,8 @@
 package com.example.android_homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -9,6 +11,12 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        RecycleViewFragment fragment = new RecycleViewFragment();
+        transaction.add(R.id.recycleViewFragment, fragment);
+        transaction.commit();
     }
 }
