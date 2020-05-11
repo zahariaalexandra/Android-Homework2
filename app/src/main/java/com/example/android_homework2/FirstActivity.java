c18bcf16e8e3b509a8516f687b17a7bbeca02d00
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-05-11 16:15:39 +0300

    Fix a malfunction and add partial functionality for a button
    Changed the fragment from static to dynamic
    Added button listener for the add button

--- app/src/main/java/com/example/android_homework2/FirstActivity.java
+++ app/src/main/java/com/example/android_homework2/FirstActivity.java
@@ -1,22 +1,48 @@
 package com.example.android_homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity{//FragmentActivity implements RecycleViewFragment.ToolbarListener {
+
+    /*EditText txtFirstName;
+    EditText txtLastName;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frameLayout, new RecycleViewFragment());
+        transaction.commit();
+
+       /* FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         RecycleViewFragment fragment = new RecycleViewFragment();
         transaction.add(R.id.recycleViewFragment, fragment);
-        transaction.commit();
+        transaction.commit();*/
+/*
+        txtFirstName = (EditText) findViewById(R.id.txtFirstName);
+        txtLastName = (EditText) findViewById(R.id.txtLastName);*/
     }
+
+    /*public void onButtonClick(String firstName, String lastName) {
+        Toast toast = Toast.makeText(getApplicationContext(), "First name: " + firstName, Toast.LENGTH_SHORT);
+        toast.show();
+    }*/
+
+    /*public void addPerson(View view)
+    {
+        String firstName = txtFirstName.getText().toString();
+        Toast toast = Toast.makeText(getApplicationContext(), "aa" + firstName, Toast.LENGTH_SHORT);
+        toast.show();
+    }*/
 }
--- app/src/main/java/com/example/android_homework2/RecycleViewFragment.java
+++ app/src/main/java/com/example/android_homework2/RecycleViewFragment.java
@@ -1,5 +1,6 @@
 package com.example.android_homework2;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,22 +8,59 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class RecycleViewFragment extends Fragment {
 
-    public RecycleViewFragment() {
-        // Required empty public constructor
+   /* private static final String TAG = "CreateUser";
+    private ToolbarListener activityCallback;*/
+
+    private EditText txtFirstName;
+    private EditText txtLastName;
+
+  /*  public interface ToolbarListener {
+        public void onButtonClick(String firstName, String lastName);
     }
 
+    public RecycleViewFragment() { }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            activityCallback = (ToolbarListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString());
+        }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recycle_view, container, false);
+        //final View view = inflater.inflate(R.layout.fragment_recycle_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_recycle_view, container, false);
+
+        txtFirstName = (EditText) view.findViewById(R.id.txtFirstName);
+        txtLastName = (EditText) view.findViewById(R.id.txtLastName);
+        //final Button btnAdd = (Button) view.findViewById(R.id.btnAdd);
+        Button btnAdd = (Button) view.findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addData();
+            }
+        });
+
+        return view;
+    }
+
+    private void addData()
+    {
+        Toast toast = Toast.makeText(getContext(), txtFirstName.getText().toString(), Toast.LENGTH_SHORT);
+        toast.show();
+        //activityCallback.onButtonClick(txtFirstName.getText().toString(), txtLastName.getText().toString());
     }
 }
